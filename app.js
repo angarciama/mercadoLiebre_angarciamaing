f9fd888194039030e147233a67b635a677042ae0
angarciama <angarciama@unal.edu.co>
2021-12-28 20:39:57 -0500

    Formularios

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
-app.get('/pepe', (req,res) => res.sendFile(path.resolve(__dirname, './views/registro.html')));
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
+
+
 app.listen(3000, () => console.log("Servidor corriendo en Puerto: 3000"));
+
+
+
+
 
 
